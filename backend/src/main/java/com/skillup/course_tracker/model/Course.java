395c65e6cbb085f5cbd1b37481edb92cb9323344
SahilRaza79656 <sahil.raza.79656@gmail.com>
2025-07-08 19:20:40 +0530

    Added Course entity

---
+++ backend/src/main/java/com/skillup/course_tracker/model/Course.java
@@ -0,0 +1,26 @@
+package com.skillup.course_tracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String platform;
+    private String url;
+
+    @Column(length = 1000)
+    private String description;
+}
