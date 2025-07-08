7356c5fee052f29e9ea8b420b505d6530646ca8d
SahilRaza79656 <sahil.raza.79656@gmail.com>
2025-07-08 19:04:23 +0530

    Spring Boot project setup with H2 and dependencies

---
+++ backend/src/test/java/com/skillup/course_tracker/CourseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillup.course_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
