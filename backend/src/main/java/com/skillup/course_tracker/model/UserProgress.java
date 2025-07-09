6c71f03f0ff21a10bc7575eca83f1d09fcedc5c2
SahilRaza79656 <sahil.raza.79656@gmail.com>
2025-07-09 17:28:19 +0530

    Created UserProgress entity and mapped it to Course

---
+++ backend/src/main/java/com/skillup/course_tracker/model/UserProgress.java
@@ -0,0 +1,27 @@
+package com.skillup.course_tracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int progress; // should be between 0 and 100
+    private String notes;
+
+    // Many UserProgress entries can be gitrelated to one Course
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+}
