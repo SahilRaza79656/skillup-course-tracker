bbf7f8eece387d2a0a08d8d5c88674e0841f4a9f
SahilRaza79656 <sahil.raza.79656@gmail.com>
2025-07-10 20:45:23 +0530

    Created Navbar component and added Bootstrap support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="container">
-       <h1>SkillUp - Course Tracker</h1>
-       <p>Welcome! Start adding and tracking your learning courses here.</p>
+      <Navbar />
+      <h1>SkillUp - Course Tracker</h1>
+      <p>Welcome! Start adding and tracking your learning courses here.</p>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar(){
+    return (
+        <nav className='navbar navbar-dark bg-dark mb-4'>
+            <div className='container'>
+                <span className='navbar-brand'>SkillUp Tracker</span>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
