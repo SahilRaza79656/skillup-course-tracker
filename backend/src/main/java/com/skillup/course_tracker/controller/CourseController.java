b306ced9861166836764675d9a16abeff7e05975
SahilRaza79656 <sahil.raza.79656@gmail.com>
2025-07-15 18:57:40 +0530

    Removed URL from origins for now, will add after UI is deployed

--- backend/src/main/java/com/skillup/course_tracker/controller/CourseController.java
+++ backend/src/main/java/com/skillup/course_tracker/controller/CourseController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/courses")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class CourseController {
 
     private final CourseService service;
